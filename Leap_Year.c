b69669492700bf5585256df4e5489c8bb123bb3e
RK <90020245+ORKTech@users.noreply.github.com>
2026-02-12 18:12:55 +0530

    Add files via upload

---
+++ Leap_Year.c
@@ -0,0 +1,19 @@
+//Libraries Included:
+#include <stdio.h>
+
+//Global variable:
+int year;
+
+//Main:
+int main() {
+	printf("Enter a year: "); //Prompt the user to enter a year
+	scanf_s("%d", &year);
+	//Check if the year is a leap year:
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+		printf("%d is a leap year.\n", year);
+	}
+	else {
+		printf("%d is not a leap year.\n", year);
+	}
+	return 0;
+}
