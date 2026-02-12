9cb4131be810f926be7fbb4ecccd2c690ac70fb3
RK <90020245+ORKTech@users.noreply.github.com>
2026-02-12 18:43:47 +0530

    Add files via upload

---
+++ Print_Input_In_Reverse_Order.c
@@ -0,0 +1,28 @@
+//Library Included:
+#include <stdio.h>
+
+//Main:
+int main()
+{
+    int num, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num == 0)
+    {
+        printf("Digits in reverse order: 0");
+    }
+    else
+    {
+        printf("Digits in reverse order: ");
+        while (num != 0)
+        {
+            digit = num % 10;
+            printf("%d ", digit);
+            num = num / 10;
+        }
+    }
+
+    return 0;
+}
